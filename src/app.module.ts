85a614b3e9c14b76511c5c20ec4da177db048625
annguyen34 <21120508@student.hcmus.edu.vn>
2025-01-06 06:35:30 +0700

    add movie schema and get API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from "@nestjs/common"
+import { AppController } from "./app.controller"
+import { AppService } from "./app.service"
+import { AuthModule } from "./auth/auth.module"
+import { MongooseModule } from "@nestjs/mongoose"
 import { ConfigModule } from "@nestjs/config"
+import { MoviesModule } from "./movies/movies.module"
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.DATABASE_URI), AuthModule,
+    MongooseModule.forRoot(process.env.DATABASE_URI),
+    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    MoviesModule
   ],
   controllers: [AppController],
   providers: [AppService]
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common"
+import { InjectModel } from "@nestjs/mongoose"
+import { Model } from "mongoose"
+import { Movie, MovieDocument } from "../models/movie.schema"
+
+@Injectable()
+export class MoviesService {
+  constructor(
+    @InjectModel(Movie.name) private movieModel: Model<MovieDocument>
+  ) {}
+
+  async findAll(page: number, limit: number): Promise<Movie[]> {
+    const skip = (page - 1) * limit
+    return this.movieModel.find().skip(skip).limit(limit).exec()
+  }
+
+  async findOne(id: string): Promise<Movie> {
+    return this.movieModel.findById(id).exec()
+  }
+}
