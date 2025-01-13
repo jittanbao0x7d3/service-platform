15664b8fc890df510fedffdfa11ac0a71cf50111
Phuc Nguyen <phuc.nguyen@c0x12c.com>
2025-01-13 15:34:09 +0700

    fix login

--- src/main.ts
+++ src/main.ts
@@ -40,6 +40,6 @@ async function bootstrap() {
   app.use(passport.session())
   app.enableCors()
 
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(process.env.PORT!)
 }
 bootstrap()
